d0c884afcbe69b6a7d5b33cb11d7eaa364deb7d1
azdharsyahputra <azdharsyahputra@gmail.com>
2026-03-09 14:12:05 +0700

    export excell

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
 	"portal-report-bi/internal/config"
-	"portal-report-bi/internal/external"
 	"portal-report-bi/internal/handler"
 	"portal-report-bi/internal/parser"
 	"portal-report-bi/internal/repository"
@@ -35,21 +30,6 @@ func main() {
 	logger := config.InitLogger()
 	defer logger.Sync()
 
-	regencyRepo := repository.NewRegencyRepository(queryServiceURL)
-
-	regencyAPIURL := os.Getenv("REGENCY_API_URL")
-	if regencyAPIURL == "" {
-		regencyAPIURL = "http://localhost:8081"
-	}
-	regencyAPIClient := external.NewRegencyAPIClient(regencyAPIURL)
-
-	regencyService := service.NewRegencyService(regencyRepo, regencyAPIClient)
-
-	regencyHandler := handler.NewRegencyHandler(
-		regencyService,
-		logger,
-	)
-
 	excelParser := parser.NewExcelParser()
 	branchRepo := repository.NewBranchCodeBankRepository(queryServiceURL)
 	reportRepo := repository.NewReportRepository(queryServiceURL)
@@ -71,33 +51,7 @@ func main() {
 
 	r := gin.Default()
 
-	routes.RegisterRoutes(r, authHandler, regencyHandler, reportHandler, importHandler, branchBankHandler, kycHandler)
-
-	r.GET("/test-regencies", func(c *gin.Context) {
-		query := "SELECT * FROM vdapp_3.regencies"
-		body, _ := json.Marshal(map[string]string{"qstr": query})
-		resp, err := http.Post(queryServiceURL, "application/json", bytes.NewBuffer(body))
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		defer resp.Body.Close()
-
-		b, _ := io.ReadAll(resp.Body)
-		if resp.StatusCode != http.StatusOK {
-			c.JSON(resp.StatusCode, gin.H{"query_service_error": string(b)})
-			return
-		}
-
-		var result interface{}
-		if err := json.Unmarshal(b, &result); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "JSON Unmarshal failed", "raw_response": string(b)})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"data": result})
-	})
-
+	routes.RegisterRoutes(r, authHandler, reportHandler, importHandler, branchBankHandler, kycHandler)
 	log.Println("server running on :8080")
 	log.Fatal(r.Run(":8080"))
 }
